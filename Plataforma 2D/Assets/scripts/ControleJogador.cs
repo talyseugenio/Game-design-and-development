94a85335c7d82b612f748d41da090c630d20d34f
TALYS EUGENIO BATISTA FERREIRA <22137@unifacef.edu.br>
2022-08-24 20:01:45 -0300

    Second Class

--- Plataforma 2D/Assets/scripts/ControleJogador.cs
+++ Plataforma 2D/Assets/scripts/ControleJogador.cs
@@ -8,24 +8,48 @@ public class ControleJogador : MonoBehaviour
     public float speed;
     public float jumpForce;
 	private bool pulando = false;
+	private Animator animator;
+	public Transform camera;
 
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator> ();
+		//definir a posiçõa inicial da câmera
+		camera.position = new Vector3(1.21f,0.0f,-10.0f);
     }
     
     void Update()
     {
         float mov = Input.GetAxisRaw("Horizontal");
-        rig.velocity = new Vector2(mov * speed, rig.velocity.y);
+		if (mov == 1) {
+			GetComponent<SpriteRenderer> ().flipX = false;
+		} else if (mov == -1) {
+			GetComponent<SpriteRenderer> ().flipX = true;
+		}
 
-        if (Input.GetKeyDown(KeyCode.Space)  && pulando == false ) {
+        rig.velocity = new Vector2(mov * speed, rig.velocity.y);
+		animator.SetFloat ("Velocidade", Mathf.Abs (mov));
+
+        if (Input.GetKeyDown(KeyCode.Space)  && pulando == false) {
             rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 			pulando = true;
+			animator.SetBool ("Pulando", true);
         }
+		float camx = rig.transform.position.x + 3;
+		if(camx < 1.21f){
+			camx = 1.21f; 
+		} 
+		if (camx > 35.29f) {
+			camx = 35.29f;
+		}
+		camera.position = new Vector3 (camx, 0.0f, -10.0f);
     }
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		pulando = false;
+		animator.SetBool ("Pulando", false);
 	}
+
+
 }
