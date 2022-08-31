42ccaff8780c9748f039a290016a13f855c536f6
TALYS EUGENIO BATISTA FERREIRA <22137@unifacef.edu.br>
2022-08-31 20:44:26 -0300

    Fourth Class

---
+++ Plataforma 2D/Assets/scripts/Fireball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+	public float mov = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = transform.position.x + (Time.deltaTime * mov);
+		float y = transform.position.y;
+		float z = transform.position.z;
+		transform.position = new Vector3 (x, y, z);
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		Destroy (gameObject);
+	}
+}
