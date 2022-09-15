81d2c087a8c6f1f68ece6326885020fe98738712
TALYS EUGENIO BATISTA FERREIRA <22137@unifacef.edu.br>
2022-09-14 22:26:33 -0300

    Trabalho Avaliativo

---
+++ TrabalhoAvaliativoEly/Assets/scripts/ControleJogador.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleJogador : MonoBehaviour
+{
+	private Rigidbody2D rig;
+	public float speed;
+	public float jumpForce;
+	private bool pulando = false;
+	private Animator animator;
+	public Transform camera;
+	//private AudioSource somPulo;
+	//private AudioSource somMoeda;
+
+	void Start()
+	{
+		rig = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator> ();
+		//definir a posiçõa inicial da câmera
+		camera.position = new Vector3(1.21f,0.0f,-10.0f);
+		//instância som 
+		//somPulo = GetComponents<AudioSource>()[1];
+		//somMoeda = GetComponents<AudioSource>()[2];
+	}
+
+	void Update()
+	{
+		float mov = Input.GetAxisRaw("Horizontal");
+		if (mov == 1) {
+			GetComponent<SpriteRenderer> ().flipX = false;
+		} else if (mov == -1) {
+			GetComponent<SpriteRenderer> ().flipX = true;
+		}
+
+		rig.velocity = new Vector2(mov * speed, rig.velocity.y);
+		animator.SetFloat ("Velocidade", Mathf.Abs (mov));
+
+		if (Input.GetKeyDown(KeyCode.Space)  && pulando == false) {
+			rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+			pulando = true;
+			animator.SetBool ("Pulando", true);
+			//somPulo.Play();
+		}
+		float camx = rig.transform.position.x + 3;
+		if(camx < 1.21f){
+			camx = 1.21f; 
+		} 
+		if (camx > 35.29f) {
+			camx = 35.29f;
+		}
+		camera.position = new Vector3 (camx, 0.0f, -10.0f);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		pulando = false;
+		animator.SetBool ("Pulando", false);
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		//somMoeda.Play ();
+		Destroy (coll.gameObject);
+	}
+
+
+}
---
+++ TrabalhoAvaliativoEly/Assets/scripts/ControleNPC.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleNPC : MonoBehaviour {
+
+	private Rigidbody2D rig;
+	private float mov = 1f;
+
+
+	// Use this for initialization
+	void Start () {
+		rig = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (mov > 0) {
+			GetComponent<SpriteRenderer> ().flipX = false;
+		} else {
+			GetComponent<SpriteRenderer> ().flipX = true;
+		}
+		rig.velocity = new Vector2 (mov, rig.velocity.y);
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			if (col.gameObject.transform.position.y > 
+				gameObject.transform.position.y + 1) {
+				Destroy (gameObject); //NPC morre
+			} else {
+				Destroy (col.gameObject); //Npc Mata
+			}
+		} else if (col.gameObject.tag == "Fire") {
+			Destroy (gameObject); //Npc morre
+		} else {
+			mov = mov * -1;
+		}
+	}
+}
